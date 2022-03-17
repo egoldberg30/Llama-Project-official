52992d58d2f5f500b0d90895f68d524acce708ce
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-03-16 22:49:45 -0400

    update files and js

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,9 @@
 // import {Carousel} from "@fancyapps/ui";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 import { Fancybox, Carousel, } from "@fancyapps/ui";
+const mainTL = gsap.timeline();
 
 window.addEventListener('load', function(){
 
@@ -11,4 +15,50 @@ window.addEventListener('load', function(){
 });
 
 
+function fadein() {
+  const tl = gsap.timeline()
+gsap.set("#wheat", {alpha:0, x:-600})
+tl.to("#wheat", {alpha:1, x:0, scrollTrigger:{trigger: "#maintain", scrub:true}})
+}
+ScrollTrigger.defaults({
+    toggleActions: "restart pause resume none",
+    markers:true
+})
 
+
+function fader() {
+  const tl = gsap.timeline()
+gsap.set("#maintain-content", {alpha:0,  x:-600})
+tl.to("#maintain-content", {alpha:1, x:0, scrollTrigger:{trigger: "#stickout", scrub:true}})
+}
+ScrollTrigger.defaults({
+    toggleActions: "restart pause resume none",
+    markers:true
+})
+
+function faderright() {
+  const tl = gsap.timeline()
+gsap.set("#stickout-image", {alpha:0,  y:400})
+tl.to("#stickout-image", {alpha:1, y:0, scrollTrigger:{trigger: "#stickout-content h2", scrub:true}})
+}
+ScrollTrigger.defaults({
+    toggleActions: "restart pause resume none",
+    markers:true
+})
+
+function sherringfadeup() {
+  const tl = gsap.timeline()
+gsap.set("#stickout-image", {alpha:0,  y:400})
+tl.to("#stickout-image", {alpha:1, y:0, scrollTrigger:{trigger: "#stickout-content h2", scrub:true}})
+}
+ScrollTrigger.defaults({
+    toggleActions: "restart pause resume none",
+    markers:true
+})
+
+
+
+mainTL.add(fadein())
+.add(fader())
+.add(sherringfadeup())
+.add(faderright());
