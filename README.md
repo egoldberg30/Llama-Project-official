9237f6c3cfd3b14cdd5ac7d8301db4d479c0d569
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-03-16 21:18:53 -0400

    repo set up

--- README.md
+++
@@ -1,2 +0,0 @@
-# Llama-Project-official
- 
---
+++ dev/js/scripts.js
@@ -0,0 +1,14 @@
+// import {Carousel} from "@fancyapps/ui";
+import { Fancybox, Carousel, } from "@fancyapps/ui";
+
+window.addEventListener('load', function(){
+
+    const myCarousel = new Carousel(document.querySelector(".carousel"), {
+        // Options
+        // 'center' : false
+      });
+
+});
+
+
+
